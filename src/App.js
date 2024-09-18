aef5d18b7829214d301d674b3970dae3377e381f
dawin.kweon@eroad.com <dawin.kweon@eroad.com>
2024-09-18 21:41:56 +1200

    improve styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+import "./App.css";
 import * as ml5 from "ml5";
 import Webcam from "react-webcam";
-import {useEffect, useRef} from "react";
+import { useEffect, useRef, useState } from "react";
 
 // ideal demo size: 335, 640
 const dimensions = {
-  width: 320, 
-  height: 240
-}
+  width: 320,
+  height: 240,
+};
 function App() {
+  const [detected, setDetected] = useState([]);
+
   const webcamRef = useRef();
   const canvasRef = useRef();
   const { width, height } = dimensions;
@@ -22,31 +24,31 @@ function App() {
       canvasRef.current.width = width;
       canvasRef.current.height = height;
 
-
       detectionInterval = setInterval(() => {
         detect();
       }, 200);
     };
 
-    const objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    const objectDetector = ml5.objectDetector("cocossd", modelLoaded);
 
     const detect = () => {
       if (webcamRef.current.video.readyState !== 4) {
-        console.warn('Video not ready yet');
+        console.warn("Video not ready yet");
         return;
       }
 
       objectDetector.detect(webcamRef.current.video, (err, results) => {
-        const ctx = canvasRef.current.getContext('2d');
-        ctx.clearRect(0, 0, width,height);
+        const ctx = canvasRef.current.getContext("2d");
+        ctx.clearRect(0, 0, width, height);
         if (results && results.length) {
+          setDetected(results);
           results.forEach((detection) => {
-            // console.log(detection);
+            ctx.strokeStyle = "#FF0000";
             ctx.beginPath();
             ctx.fillStyle = "#FF0000";
             const { label, x, y, width, height } = detection;
-            ctx.fillText(label, x/2 + 5, y/2 + 10);
-            ctx.rect(x/2, y/2, width/2, height/2);
+            ctx.fillText(label, x / 2 + 5, y / 2 + 10);
+            ctx.rect(x / 2, y / 2, width / 2, height / 2);
             ctx.stroke();
           });
         }
@@ -57,15 +59,23 @@ function App() {
       if (detectionInterval) {
         clearInterval(detectionInterval);
       }
-    }
-
+    };
   }, [width, height]);
 
+  const detectedGroups = Object.groupBy(detected, ({label}) => label);
+  Object.values(detectedGroups).sort((a,b) => { return a[0].label > b[0].label});
+  
   return (
+    <div style={{width: dimensions.width}}>
+      <Webcam ref={webcamRef} className="webcam" />
+      <canvas ref={canvasRef} className="canvas" />
       <div>
-        <Webcam ref={webcamRef} className="webcam"/>
-        <canvas ref={canvasRef} className="canvas"/>
+        {Object.values(detectedGroups).map((group) => (
+          <div className="card">{group[0].label} ({group.length})</div>
+        ))}
+        <button className="button-24">Submit ({detected.length} asset)</button>
       </div>
+    </div>
   );
 }
 
